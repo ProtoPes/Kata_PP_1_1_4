adb9d12e4677cbbdc77a4a4f6165c91c8da218e6
ProtoPes <ebu.krota@gmail.com>
2023-03-14 13:14:09 +0300

    1. Added "rollback" method to any connection, which manipulates data in DB.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,6 +2,8 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import static jm.task.core.jdbc.util.Util.newSession;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.util.List;
@@ -14,43 +16,63 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = newSession()) {
+        Session session = newSession();
+        try {
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
                     "name VARCHAR(256), lastName VARCHAR(256), age TINYINT CHECK (age > 0))").executeUpdate();
             session.getTransaction().commit();
             System.out.println("Table \"users\" was created!");
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
         }
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
-        try (Session session = newSession()) {
+        Session session = newSession();
+        try {
             session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
             session.getTransaction().commit();
             System.out.println("Table \"users\" was deleted!");
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
         }
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = newSession()) {
+        Session session = newSession();
+        try {
             session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
             System.out.printf("User with name %s - was added to the DataBase.\n", name);
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
         }
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = newSession()) {
+        Session session = newSession();
+        try {
             session.beginTransaction();
             session.delete(session.get(User.class, id));
             session.getTransaction().commit();
             System.out.printf("User with id %d was deleted!\n", id);
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
         }
+        session.close();
     }
 
     @Override
@@ -66,11 +88,16 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = newSession()) {
+        Session session = newSession();
+        try {
             session.beginTransaction();
             session.createQuery("DELETE User").executeUpdate();
             session.getTransaction().commit();
             System.out.println("Table \"users\" was cleaned!");
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
         }
+        session.close();
     }
 }
